b6f8c00386971b63fbc78caf9e5c339bffdc916f
LovesAsuna <qq625924077@gmail.com>
2020-09-26 23:01:01 +0800

    [Refactor] 更高性能的文件更新

--- src/main/java/org/sct/easylib/util/plugin/FileUpdate.java
+++ src/main/java/org/sct/easylib/util/plugin/FileUpdate.java
@@ -4,16 +4,16 @@ import org.bukkit.Bukkit;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.sct.easylib.EasyLib;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * @author LovesAsuna
@@ -27,65 +27,40 @@ public class FileUpdate {
             if (System.getProperty("os.name").contains("Window")) {
                 jarPath = jarPath.replaceFirst("/", "");
             }
-            FileSystem system = null;
+            JarFile jarFile = new JarFile(new File(jarPath));
 
-            system = FileSystems.newFileSystem(Paths.get(jarPath), null);
-            String savePath = Paths.get(jarPath).getParent().toString() + File.separator + instance.getName() + "_" + fileName;
-
-            File originFile = new File(savePath);
             File currentFile = new File(path + File.separator + fileName);
 
-            deleteFile(originFile, savePath);
-            Files.copy(system.getPath(fileName), Paths.get(savePath));
+            Optional<JarEntry> optional = jarFile.stream().parallel().filter(s -> s.getName().equalsIgnoreCase(fileName)).findFirst();
+            if (!optional.isPresent()) {
+                throw new IllegalArgumentException("wrong file");
+            }
+            Reader reader = new InputStreamReader(jarFile.getInputStream(optional.get()));
 
-            YamlConfiguration orign = YamlConfiguration.loadConfiguration(originFile);
-            Set<String> orignKeys = orign.getConfigurationSection("").getKeys(true);
+            YamlConfiguration orign = YamlConfiguration.loadConfiguration(reader);
+            Set<String> originKeys = Objects.requireNonNull(orign.getConfigurationSection("")).getKeys(true);
 
             YamlConfiguration current = YamlConfiguration.loadConfiguration(currentFile);
-            Set<String> currentKeys = orign.getConfigurationSection("").getKeys(true);
 
-            int currentsize = getSize(current);
+            int currentSize = getSize(current);
             int originSize = getSize(orign);
-            if (currentsize < originSize) {
-                orignKeys.stream().forEach(orignString -> {
-                    AtomicBoolean contains = new AtomicBoolean(false);
-                    Set<String> keys = instance.getConfig().getConfigurationSection("").getKeys(true);
-                    keys.parallelStream().forEach(string -> {
-                        if (orignString.equals(string)) {
-                            contains.set(true);
-                        }
-                    });
-                    if (!contains.get()) {
-                        current.set(orignString, orign.get(orignString));
+            if (currentSize < originSize) {
+                originKeys.forEach(originString -> {
+                    Set<String> keys = Objects.requireNonNull(current.getConfigurationSection("")).getKeys(true);
+                    if (keys.parallelStream().noneMatch(s -> s.equals(originString))) {
+                        current.set(originString, orign.get(originString));
                     }
                 });
                 current.save(currentFile);
             }
-            Bukkit.getScheduler().runTaskLaterAsynchronously(EasyLib.getInstance(), () -> {
-                deleteFile(originFile, savePath);
-            }, 20L);
         } catch (IOException e) {
-             e.printStackTrace();
+            e.printStackTrace();
             Bukkit.getConsoleSender().sendMessage("§7[§eEasyLib§7]§c自动更新配置文件时出错!");
         }
     }
 
-    private static boolean isSection(String path) {
-        return EasyLib.getInstance().getConfig().getConfigurationSection(path) == null ? false : true;
-    }
-
     private static int getSize(Configuration configuration) {
-        return configuration.getConfigurationSection("").getKeys(true).size();
+        return Objects.requireNonNull(configuration.getConfigurationSection("")).getKeys(true).size();
     }
 
-    private static boolean deleteFile(File file, String savePath) {
-        if (file.exists()) {
-            try {
-                Files.delete(Paths.get(savePath));
-            } catch (IOException e) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
